d6a8c9db53cdc3474de2f7c7c2d51d13586032ba
aaronshawz <123578708+aaronshawz@users.noreply.github.com>
2024-06-09 22:17:15 -0500

    lgtm
    lgtm

---
+++ script.js
@@ -0,0 +1,15 @@
+var video = document.getElementById('background-video');
+var clicks = 0
+
+document.getElementById('mute-button').addEventListener('click', function() {
+    if (clicks == 0) {
+        video.play();
+        video.muted = false;
+        return
+    }
+    if (video.muted == true) {
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+});
